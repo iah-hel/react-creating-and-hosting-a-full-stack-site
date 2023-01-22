6b1dfd7c91eab3f8d95878207b028da9ff63c090
iah-hel <calvillohugo@hotmail.com>
2023-01-22 12:45:01 -0600

    changes for 1.6 secction
    Adds article-content component
    Adds Logic to render article  in  ArticlePage

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,22 @@
+//localhost:3000/articles/learn-code
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlePage = () =>{
+    // const params = useParams();
+    // const { articleId } = params;
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
+
     return (
-        <h1>This is the article page!</h1>
+        <>
+            <h1>{article.title}</h1>
+            {
+                article.content.map((paragraph,index)  => (
+                    <p key={index}>{paragraph}</p>
+                ))
+            }
+        </>
     )
 }
 
