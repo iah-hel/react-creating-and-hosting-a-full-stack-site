428e38bc370f82ff899b447e9d2ea8c3692718b9
iah-hel <calvillohugo@hotmail.com>
2023-01-23 11:34:59 -0600

    Adds Changes for 2.7
    Adds endpoint to adds comments to a specific article

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,24 @@
 import express, { response } from 'express';
 
+//Fake database
 let articlesInfo= [{
     name:'learn-react',
     upvotes:0,
+    comments:[],
 },{
     name:'learn-node',
     upvotes:0,
+    comments:[],
 },{
     name:'mongodb',
     upvotes:0,
+    comments:[],
 }]
 
 const app = express();
 app.use(express.json());
 
+//EndPoints
 
 app.put('/api/articles/:name/upvotes',(req,res)=>{
     const {name} = req.params;
@@ -26,6 +31,23 @@ app.put('/api/articles/:name/upvotes',(req,res)=>{
         res.send('That article doesn\'t exist');
     }
 })
+
+app.post('/api/articles/:name/comments',(req,res)=>{
+    const {name} = req.params;
+    const {postedBy,text} = req.body;
+
+    const article = articlesInfo.find(article=> article.name === name);
+
+    if(article){
+        article.comments.push({postedBy,text});
+        res.send(article.comments);
+    }else{
+        res.send('That article doesn\'t exist');
+    }
+
+})
+
+
 
 app.listen(8000,()=>{
     console.log('Server is listening on port 8000')
