1b53d3adc87844aa9f3cd0e87635fa31eabc2cc8
iah-hel <calvillohugo@hotmail.com>
2023-01-23 11:10:32 -0600

    Adds changes for 2.5

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,31 @@
-import express from 'express';
+import express, { response } from 'express';
+
+let articlesInfo= [{
+    name:'learn-react',
+    upvotes:0,
+},{
+    name:'learn-node',
+    upvotes:0,
+},{
+    name:'mongodb',
+    upvotes:0,
+}]
 
 const app = express();
-
-//Tells express that the request  has a Json body and it should try to parse
 app.use(express.json());
 
-app.get('/hello/:name/goodbye/:otherName',(req,res) => {
-    const { name } = req.params;
-    const { otherName } = req.params;
-    console.log(req.params)
-    res.send(`Hello ${name}!! \n Good bye ${otherName}`);
-});
 
-app.post('/hello',(req,res) => {
-    console.log(req.body)
-    res.send(`Hello ${req.body.name}!!`);
-});
-
+app.put('/api/articles/:name/upvotes',(req,res)=>{
+    const {name} = req.params;
+    const article = articlesInfo.find(article=> article.name === name);
+    
+    if(article){
+        article.upvotes += 1;
+        res.send(`The  ${name} article now has ${article.upvotes} upvotes`);
+    }else{
+        res.send('That article doesn\'t exist');
+    }
+})
 
 app.listen(8000,()=>{
     console.log('Server is listening on port 8000')
