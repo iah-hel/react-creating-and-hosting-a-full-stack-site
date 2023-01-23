16ef1c0f71c76216238ae336f78704972e310cc9
iah-hel <calvillohugo@hotmail.com>
2023-01-23 09:36:14 -0600

    Add changes for 1.9
    Creates 404 page
    Adds 404 page to the route
    Adds logic to show 404 page in articles

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,17 @@
 //localhost:3000/articles/learn-code
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
+import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () =>{
     // const params = useParams();
     // const { articleId } = params;
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
+    
+    if(!article){
+        return <NotFoundPage />   
+    }
 
     return (
         <>
