53d3ce8630e3806c1a43030f7d4cf65e9cbd968d
iah-hel <calvillohugo@hotmail.com>
2023-01-23 10:54:15 -0600

    Adds changes for 2.4

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,13 +5,16 @@ const app = express();
 //Tells express that the request  has a Json body and it should try to parse
 app.use(express.json());
 
-app.get('/hello',(req,res) => {
-    res.send("Hello!");
+app.get('/hello/:name/goodbye/:otherName',(req,res) => {
+    const { name } = req.params;
+    const { otherName } = req.params;
+    console.log(req.params)
+    res.send(`Hello ${name}!! \n Good bye ${otherName}`);
 });
 
 app.post('/hello',(req,res) => {
     console.log(req.body)
-    res.send(`Hello ${req.body.name}!`);
+    res.send(`Hello ${req.body.name}!!`);
 });
 
 
