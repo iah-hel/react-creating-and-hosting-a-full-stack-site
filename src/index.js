0526bc367e460783d252c7176063ba60d9f82b1b
iah-hel <calvillohugo@hotmail.com>
2023-01-24 11:24:02 -0600

    Add changes for 5.3

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfeGGsTW38hSVLHAFjYRDXBcQXFYd4lak",
+  authDomain: "my-react-blog-2db6d.firebaseapp.com",
+  projectId: "my-react-blog-2db6d",
+  storageBucket: "my-react-blog-2db6d.appspot.com",
+  messagingSenderId: "886917328250",
+  appId: "1:886917328250:web:b9ce0334581f8ba27d7ac3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
